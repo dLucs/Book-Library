79c0d5757ecaf1bff880dd08591cb15b1355f462
dLucs <lucas_61@hotmail.es>
2022-06-28 18:47:26 +0200

    continued with Read/unread buttons and started with the add new book functionality

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-let readButton = document.getElementById("book__read-button");
-let clickCount = 0;
+const readButtons = document.querySelectorAll(".book__read-button");
+const bookList = document.querySelector(".booklist");
 
-readButton.addEventListener("click", () => {
-  readButton.classList.toggle("clicked");
-  clickCount += 1;
+//-------------------Toggle Read or Unread Button --------------------------
 
-  if (clickCount % 2 == 0) {
-    readButton.textContent = "Unread";
-  } else {
-    readButton.textContent = "Read";
-  }
+readButtons.forEach((readButton) => {
+  let clickCount = 0;
+
+  readButton.addEventListener("click", (e) => {
+    e.target.classList.toggle("clicked");
+
+    clickCount += 1;
+
+    if (clickCount % 2 == 0) {
+      readButton.textContent = "Unread";
+    } else {
+      readButton.textContent = "Read";
+    }
+  });
+});
+
+// ---------------------- Add a new book to list -------------------------------
+
+bookList.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //add a new book
 });
